b72d9d53c7c3555eb1a0c5481934130961d71dd6
hashim <medoalkhateeb@ymail.com>
2024-02-21 23:53:33 +0200

    update sh

--- src/common/scripts/cmake_install.sh
+++ src/common/scripts/cmake_install.sh
@@ -17,8 +17,8 @@ sudo apt-get update \
 
 wget  https://github.com/Kitware/CMake/releases/download/v3.24.1/cmake-3.24.1-Linux-$1.sh \
       -q -O /tmp/cmake-install.sh \
-      && chmod u+x /tmp/cmake-install.sh \
-      && mkdir /opt/cmake-3.24.1 \
-      && /tmp/cmake-install.sh --skip-license --prefix=/opt/cmake-3.24.1 \
-      && rm /tmp/cmake-install.sh \
-      && ln -s /opt/cmake-3.24.1/bin/* /usr/local/bin
+      && sudo chmod u+x /tmp/cmake-install.sh \
+      && sudo mkdir /opt/cmake-3.24.1 \
+      && sudo /tmp/cmake-install.sh --skip-license --prefix=/opt/cmake-3.24.1 \
+      && sudo rm /tmp/cmake-install.sh \
+      && sudo ln -s /opt/cmake-3.24.1/bin/* /usr/local/bin
